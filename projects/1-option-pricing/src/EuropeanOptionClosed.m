4772b045a17704a9c4f80bbf4d29f79432badb23
jacopo-raffaeli <jacoporaffaeli@gmail.com>
2025-09-27 23:14:01 +0200

    Add EuropeanOptionClosed.m

---
+++ projects/1-option-pricing/src/EuropeanOptionClosed.m
@@ -0,0 +1,14 @@
+function optionPrice = EuropeanOptionClosed(F0, K, B, T, sigma, flag)
+    % EuropeanOptionClosed computes the closed-form solution for European option pricing
+    % using the Black-Scholes formula.
+    % Inputs:
+    %   F0    - Current forward price of the underlying asset
+    %   K     - Strike price of the option
+    %   B     - Discount factor to maturity
+    %   T     - Time to maturity (in years)
+    %   sigma - Volatility of the underlying asset (annualized)
+    %   flag  - '+1' for call option, '-1' for put option
+
+    optionPrice = 
+    
+end
