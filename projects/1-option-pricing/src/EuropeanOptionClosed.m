f51881eba69a8c9ea50ceb1fed96c77647dc2563
jacopo-raffaeli <jacoporaffaeli@gmail.com>
2025-09-28 17:57:02 +0200

    Update documentation for EuropeanOptionClosed.m and complete option price computation

--- projects/1-option-pricing/src/EuropeanOptionClosed.m
+++ projects/1-option-pricing/src/EuropeanOptionClosed.m
@@ -4,11 +4,21 @@ function optionPrice = EuropeanOptionClosed(F0, K, B, T, sigma, flag)
     % Inputs:
     %   F0    - Current forward price of the underlying asset
     %   K     - Strike price of the option
-    %   B     - Discount factor to maturity
+    %   B     - Annualized continuously compounded risk-free rate of return over life of the option
     %   T     - Time to maturity (in years)
     %   sigma - Volatility of the underlying asset (annualized)
     %   flag  - '+1' for call option, '-1' for put option
 
-    optionPrice = 
+    % Compute option price
+    [callPrice, putPrice] = blkprice(F0, K, B, T, sigma);
     
+    % Select the appropriate price based on the flag
+    if flag == 1
+        optionPrice = callPrice;
+    elseif flag == -1
+        optionPrice = putPrice;
+    else
+        error('Invalid flag. Use +1 for call option and -1 for put option.');
+    end
+
 end
