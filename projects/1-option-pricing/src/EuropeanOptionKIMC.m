f1b3c641f4cb0d302feae9e313d8a6ec0b6ffe04
jacopo-raffaeli <jacoporaffaeli@gmail.com>
2025-10-03 16:50:12 +0200

    Fix KI MC pricing, Add path-wise pricing algorithm to manage memory limitations

--- projects/1-option-pricing/src/EuropeanOptionKIMC.m
+++ projects/1-option-pricing/src/EuropeanOptionKIMC.m
@@ -1,4 +1,4 @@
-function optionPrice = EuropeanOptionKIMC(S0, K, KI, r, q, T, sigma, N)
+function optionPrice = EuropeanOptionKIMC(S0, K, KI, r, q, T, sigma, nSim, nStep)
     % EuropeanOptionKIMC computes the price of a European knock-in call 
     % option using Monte Carlo simulation.
 
@@ -16,20 +16,23 @@ function optionPrice = EuropeanOptionKIMC(S0, K, KI, r, q, T, sigma, N)
     %   optionPrice - The computed price of the call option
 
     % Time increment
-    dt = T / N;
+    dt = T / nStep;
 
     B = exp(-r * T);
 
-    % Simulate the underlying asset price at maturity
-    nSim = 1e5;
-    Z = randn(nSim, N);
-    Z = [Z; -Z];
-    F = S0 * exp(cumsum((r - q - 0.5 * sigma^2) * dt + sigma * sqrt(dt) * Z, 2));
-    FT = F(:, end);
+    % Simulate the underlying asset price at maturity path-wise (for memory limitations)
+    Smax = zeros(1, nSim);
+    ST = zeros(1, nSim);
+    for i= 1:nSim
+        Z = randn(1, nStep);
+        S = S0 * exp(cumsum((r - q - 0.5 * sigma^2) * dt + sigma * sqrt(dt) * Z, 2));
+        Smax(i) = max(S);
+        ST(i) = S(end);
+    end
 
     % Check if the knock-in barrier was breached
-    idxKI = max(F,[],2) >= KI;
-    payoffs = max(FT - K, 0) .* idxKI;
+    idxKI = Smax >= KI;
+    payoffs = max(ST - K, 0) .* idxKI;
 
     % Discount the expected payoff to present value
     optionPrice = B * mean(payoffs);
