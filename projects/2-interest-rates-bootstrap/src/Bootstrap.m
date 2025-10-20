6c7906c6e29cafdca666bd395b9963ac1d94ab84
jacopo-raffaeli <jacoporaffaeli@gmail.com>
2025-10-20 11:27:52 +0200

    Implemented curve bootsrap and zero rate retrivial

--- projects/2-interest-rates-bootstrap/src/Bootstrap.m
+++ projects/2-interest-rates-bootstrap/src/Bootstrap.m
@@ -1,90 +1,86 @@
 function [dates, discounts] = Bootstrap(datesSet, ratesSet)
 
 %% Year fraction conventions
-% Act/365 
-Act365 = 3; 
-% Act/360
-Act360 = 2;
-% Eur 30/360
-Eur360 = 6; 
+ACT_365 = 3; 
+ACT_360 = 2;
+EUR_30_360 = 6; 
 
 %% Input data
-% Settlement data
-settlementDate = datesSet.settlement;
-settlementRate = 1;
+% Settlement
+settleDate = datesSet.settlement;
+settleDF = 1;
 
-% Deposit data
-depositDates = datesSet.deposits;
-depositRates = mean(ratesSet.deposits, 2);
+% Deposits
+depoDates = datesSet.deposits;
+depoRates = mean(ratesSet.deposits, 2);
 
-% STIR Futures data
-futureDates = datesSet.futures;
-futureRates = mean(ratesSet.futures, 2);
+% STIR Futures
+futDates = datesSet.futures;
+futRates = mean(ratesSet.futures, 2);
 
-% Swap data
+% Swaps
 swapDates = datesSet.swaps;
 swapRates = mean(ratesSet.swaps, 2);
 
-%% Initialize curve
-depositNum = sum(depositDates <= futureDates(1,1));
-futureNum = sum(futureDates(:,2) <= swapDates(2));
-swapNum = numel(swapDates) - 1;
-curveLength = 1 + depositNum + futureNum + swapNum;
+%% Initialize curve structure
+numDepo = sum(depoDates <= futDates(1,1));
+numFut  = sum(futDates(:,2) <= swapDates(2));
+numSwap = numel(swapDates) - 1;
+curveLength = 1 + numDepo + numFut + numSwap;
 
 dates = NaT(curveLength, 1);
 discounts = zeros(curveLength, 1);
 
-dates(1) = settlementDate;
-discounts(1) = settlementRate;
+dates(1) = settleDate;
+discounts(1) = settleDF;
 
-%% Short curve
-
-% ...
-for i = 1:depositNum
-    T = depositDates(i);
-    L = depositRates(i);
-    delta = yearfrac(settlementDate, T, Act360);
-    B = 1 / (1 + delta * L);
-    dates(i + 1) = T;
-    discounts(i + 1) = B;
+%% Short end (Deposits)
+for i = 1:numDepo
+    endDate = depoDates(i);
+    depoRate = depoRates(i);
+    delta = yearfrac(settleDate, endDate, ACT_360);
+    df = 1 / (1 + delta * depoRate);
+    dates(i + 1) = endDate;
+    discounts(i + 1) = df;
 end
 
-%% Middle curve
+%% Middle end (STIR Futures)
+for i = 1:numFut
+    startDate = futDates(i, 1);
+    endDate = futDates(i, 2);
+    futRate = futRates(i);
+    delta = yearfrac(startDate, endDate, ACT_360);
+    dfForward = 1 / (1 + delta * futRate);
 
-% ...
-for i = 1:futureNum
-    T = futureDates(i, 1);
-    S = futureDates(i, 2);
-    L = futureRates(i);
-    delta = yearfrac(T, S, Act360);
-    B_T_S = 1 / (1 + delta * L);
-    date1 = dates(i + depositNum - 1);
-    date2 = dates(i + depositNum);
-    zrate1 = -log(discounts(i + depositNum - 1)) / yearfrac(settlementDate, date1, Act365);
-    zrate2 = -log(discounts(i + depositNum)) / yearfrac(settlementDate, date2, Act365);
-    z_T = interp1([date1, date2], [zrate1, zrate2], T, 'linear', 'extrap'); 
-    B_T = exp(-z_T * yearfrac(settlementDate, T, Act365));
-    B_S = B_T * B_T_S;
-    dates(i + 1 + depositNum) = S;
-    discounts(i + 1 + depositNum) = B_S;
+    knownDates = dates(2:i + numDepo);
+    knownRates = -log(discounts(2:i + numDepo)) ./ yearfrac(settleDate, knownDates, ACT_365);
+    zStart = interp1(knownDates, knownRates, startDate, 'linear', 'extrap');
+    dfStart = exp(-zStart * yearfrac(settleDate, startDate, ACT_365));
+
+    dfEnd = dfStart * dfForward;
+    dates(i + 1 + numDepo) = endDate;
+    discounts(i + 1 + numDepo) = dfEnd;
 end
 
-%% Long curve
+%% Long end (Swaps)
+for i = 2:(numSwap + 1)
+    swapMat = swapDates(i);
+    swapRate = swapRates(i);
 
-% ...
-for i = 2:(swapNum + 1)
-    S = swapDates(i);
-    c = swapRates(i);
     payDates = swapDates(1:i);
-    deltas = yearfrac([settlementDate; payDates(1:end - 1)], payDates, Eur360);
-    zdates = dates(2:i + depositNum + futureNum - 1);
-    zrates = -log(discounts(2:i + depositNum + futureNum - 1)) ./ yearfrac(settlementDate, zdates, Act365);
-    payRates = interp1(zdates, zrates, payDates(1:end - 1), 'linear', 'extrap');
-    payB = exp(-payRates .* yearfrac(settlementDate, payDates(1:end - 1), Act365));
-    BPV = sum(deltas(1:end - 1) .* payB);
-    B_S = (1 - c * BPV) / (1 + deltas(end) * c);
-    dates(i + depositNum + futureNum) = S;
-    discounts(i + depositNum + futureNum) = B_S;
+    deltaPay = yearfrac([settleDate; payDates(1:end - 1)], payDates, EUR_30_360);
+
+    knownDates = dates(2:i + numDepo + numFut - 1);
+    knownRates = -log(discounts(2:i + numDepo + numFut - 1)) ./ yearfrac(settleDate, knownDates, ACT_365);
+
+    interpRates = interp1(knownDates, knownRates, payDates(1:end - 1), 'linear', 'extrap');
+    payDF = exp(-interpRates .* yearfrac(settleDate, payDates(1:end - 1), ACT_365));
+
+    bpv = sum(deltaPay(1:end - 1) .* payDF);
+    dfMat = (1 - swapRate * bpv) / (1 + deltaPay(end) * swapRate);
+
+    dates(i + numDepo + numFut) = swapMat;
+    discounts(i + numDepo + numFut) = dfMat;
 end
 
 end
---
+++ projects/2-interest-rates-bootstrap/src/zeroRates.m
@@ -0,0 +1,7 @@
+function zRates = zeroRates(dates, discounts)
+
+ACT_365 = 3;
+settleDate = dates(1);
+deltas = yearfrac(settleDate, dates(2:end), ACT_365);
+zRates = -log(discounts(2:end)) ./ deltas;
+zRates = [0; zRates];
