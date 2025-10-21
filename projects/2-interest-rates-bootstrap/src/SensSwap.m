904bc263e4027ad87207f21f13dc54ea5dd7f105
jacopo-raffaeli <jacoporaffaeli@gmail.com>
2025-10-21 10:33:08 +0200

    Refactor SensSwap function to streamline NPV calculations and improve clarity

--- projects/2-interest-rates-bootstrap/src/SensSwap.m
+++ projects/2-interest-rates-bootstrap/src/SensSwap.m
@@ -2,30 +2,28 @@ function [DV01, BPV, DV01_z] = SensSwap(setDate, fixedLegPaymentDates, fixedRate
     
     EUR_30_360 = 6;
     ACT_365 = 3;
-    deltas = yearfrac([setDate, fixedLegPaymentDates(1:end - 1)], fixedLegPaymentDates, EUR_30_360);
+    swapType = -1;
+    bps = 1e-4;
     
     % DV01
     fixedLegDisc = interp1(dates, discounts, fixedLegPaymentDates, 'linear', 'extrap');
-    fixedLegNPV = fixedRate * sum(deltas .* fixedLegDisc);
-    floatingLegNPV = 1 - fixedLegDisc(end);
-    npv = fixedLegNPV - floatingLegNPV; 
-
+    npv = SwapNPV(setDate, fixedLegPaymentDates, fixedLegDisc, fixedRate, swapType);
+   
     fixedLegDiscShift = interp1(dates, discounts_DV01, fixedLegPaymentDates, 'linear', 'extrap');
-    fixedLegNPVShift = fixedRate * sum(deltas .* fixedLegDiscShift);
-    floatingLegNPVShift = 1 - fixedLegDiscShift(end);
-    npvShift = fixedLegNPVShift - floatingLegNPVShift;
+    npvShift = SwapNPV(setDate, fixedLegPaymentDates, fixedLegDiscShift, fixedRate, swapType);
 
     DV01 = abs(npv - npvShift);
 
     % BPV
-    BPV = sum(deltas .* fixedLegDisc) * 1e-4;
+    deltas = yearfrac([setDate, fixedLegPaymentDates(1:end - 1)], fixedLegPaymentDates, EUR_30_360);
+    BPV = sum(deltas .* fixedLegDisc) * bps;
 
     % DV01 zero rates    
-    fixedLegZeroRates = - log(fixedLegDisc) ./ yearfrac(setDate, fixedLegPaymentDates, ACT_365);
-    fixedLegDiscShift = exp(-(fixedLegZeroRates + 1e-4) .* yearfrac(setDate, fixedLegPaymentDates, ACT_365));
-    fixedLegNPVShift = fixedRate * sum(deltas .* fixedLegDiscShift);
-    floatingLegNPVShift = 1 - fixedLegDiscShift(end);
-    npvShift = fixedLegNPVShift - floatingLegNPVShift;
+    fixedLegDisc = interp1(dates, discounts, fixedLegPaymentDates, 'linear', 'extrap');
+    npv = SwapNPV(setDate, fixedLegPaymentDates, fixedLegDisc, fixedRate, swapType);
+
+    fixedLegDiscShift =  fixedLegDisc .* exp(-bps * yearfrac(setDate, fixedLegPaymentDates, ACT_365));
+    npvShift = SwapNPV(setDate, fixedLegPaymentDates, fixedLegDiscShift, fixedRate, swapType);
 
     DV01_z = abs(npv - npvShift);
 
